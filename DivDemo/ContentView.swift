0cd1ff79a7f5e48850fd2c983153a1e1b2bab3b9
Kevin Filer <kcf2112@gmail.com>
2022-03-13 11:22:53 -0400

    Initial Commit

---
+++ DivDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DivDemo
+//
+//  Created by Kevin Filer on 3/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DivDemo/DivDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivDemoApp.swift
+//  DivDemo
+//
+//  Created by Kevin Filer on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DivDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
